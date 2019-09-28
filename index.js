e8b0b7a90465c6c152ca10a2e78b518772094913
MuffOk <55667358+MuffOk@users.noreply.github.com>
2019-09-28 21:37:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+const token = 'NjI1MzIwMjAxOTM1NjUwODE3.XYeiyA.xtkTOLL9Ni1kmrk4RbOXcY9FAmk';
+
 const PREFIX = 'm!';
 
 const quoteResponses = ["It's Muffin Time!", "I wanna die!", "Why wont you let me die!", "Hey Joey!, Wanna eat me?", "Anybody want a muffin?", "Hey, somebody kill me!"];
 
 bot.on('ready', () =>{
     console.log('This bot is online!');
-});
+})
 
 
 bot.on('message', msg=>{
@@ -20,8 +22,15 @@ bot.on('message', msg=>{
 
             msg.channel.send(response).then().catch(console.error);
         break;
+        case 'help':
+            var embed = new Discord.RichEmbed()
+                .setTitle("Commands List")
+                .addField("**Fun Commands**","m!quote")
+                .setFooter("Use m!help and any command after to find info about that command")
+            msg.channel.sendEmbed(embed);
+        break;
     }
-});
+})
 
 
 
